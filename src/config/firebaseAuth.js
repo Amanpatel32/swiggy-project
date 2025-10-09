ac059ba3916e5adcbbcf7581708f227ee80e17d2
amanworkspace2005 <amanworkspace2005@gmail.com>
2025-10-09 21:58:34 +0530

    Update code

--- src/config/firebaseAuth.js
+++ src/config/firebaseAuth.js
@@ -6,15 +6,7 @@ import {  getAuth , GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDnJDB9YGtuolKK1Z8LarC7rHU8T7_wzXE",
-  authDomain: "swiggy-project-c.firebaseapp.com",
-  projectId: "swiggy-project-c",
-  storageBucket: "swiggy-project-c.firebasestorage.app",
-  messagingSenderId: "899423121367",
-  appId: "1:899423121367:web:92e3108f081a2048982592",
-  measurementId: "G-5VE8ZGKKQK"
-};
+const firebaseConfig = JSON.parse(import.meta.env.VITE_KEY) ;
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
